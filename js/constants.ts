988177f7011f102765b4c451ff025dab4baa27d2
Rick yang Tongxue <singloo.yang@gmail.com>
2019-07-02 10:28:39 +0800

    rm

--- js/constants.ts
+++
@@ -1,8 +0,0 @@
-let host = 'https://timvel.com';
-if (__DEV__) {
-  host = 'https://dev.timvel.com';
-}
-
-export const HOST = host;
-export const API_V1 = host + '/api/v1';
-export const darkSkyKey = 'fd795ee8e6b513b308235e81ed089f02';
